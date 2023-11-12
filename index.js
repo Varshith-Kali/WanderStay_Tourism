d9cceb7e5bd34fca3308330a83ad895265feaf5e
Varshith-Kali <pulivarshit@gmail.com>
2023-11-12 12:06:47 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const port = 3000;
+
+app.get('/', function(req,res) {
+    res.sendFile(__dirname + "/index1.html");
+});
+
+app.post('/', (req, res) => {
+    res.sendFile(__dirname + "/.html")
+})
+
+
+app.listen(port);
+
